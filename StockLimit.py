e035fe090957227a4e344616cb66e2d6381bf69b
unknown <>
2017-08-13 18:52:32 +0800

    code save

---
+++ StockLimit.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+import requests
+import json
+import StockIO
+import StockConfig
+import numpy as np
+from StockDownloader import write_stock_pool
+
+
+
+def limit(stock_list, output_file='',limit_point=3):
+    result = []
+    for stock in stock_list:
+        try:
+            kline = StockIO.get_kline(stock.stock_code, kline_type=StockConfig.kline_type_day)
+        except Exception as e:
+            print(e)
+            continue
+
+        close = kline[:, 2].astype(np.float)
+        cur = close[-1]
+        # 获取最低价和最高价
+        up = cur * (1 + limit_point / 100)
+        down = cur * (1 - limit_point / 100)
+        result.append((stock.stock_code, down, up, stock.stock_name))
+
+    path = '{root}/{name}'.format(root=StockConfig.path_track, name=output_file)
+    with open(path, mode='w', encoding='utf-8') as f:
+        for item in result:
+            print(item[0], item[1], item[2], item[3])
+            f.write("{}, {}, {}, {}, {}\n".format(item[0], '%05.2f' % item[1], '%05.2f' % item[2], item[3], '(* 振荡)'))
+
+
+if __name__ == '__main__':
+    limit(StockIO.get_stock('level_1'), 'level_1_track.txt')
+
